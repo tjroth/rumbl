a294a3401f3cb9846de96d6c584ebe1e16e0b1a9
Todd Roth <tjroth@mac.com>
2016-08-16 17:07:09 -0400

    first commit

---
+++ web/models/video.ex
@@ -0,0 +1,24 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields, @optional_fields) #[:url, :title, :description])
+    #|> validate_required([:url, :title, :description])
+  end
+end
